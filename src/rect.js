f2becc7f17e3cee8300a3f3b08cba15470a4e893
Oleh Helgi <povic@ukr.net>
2023-11-08 22:37:12 +0200

    added rect.js

---
+++ src/rect.js
@@ -0,0 +1,17 @@
+/**
+ * Returns an area of a rectangle given by width and height.
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {number}
+ *
+ * @example:
+ *   5, 10 => 50
+ *   5, 5  => 25
+ */
+
+function getRectangleArea(width, height) {
+  return width * height;
+}
+getRectangleArea(5, 10);
+getRectangleArea(5, 5);
